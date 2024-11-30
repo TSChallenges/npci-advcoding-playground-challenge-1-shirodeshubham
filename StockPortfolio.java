28c58fcb0fa17b059ec3e0e42b646c3d9a0652a3
shirodess20-extc <shirodess20.extc@coep.ac.in>
2024-11-30 15:45:46 +0530

    Playground Challenge-1

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -5,25 +5,36 @@ class StockPortfolio {
     private double buyingPrice;
     private double currentPrice;
 
+    private double highestPrice;
+
     // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
-        // TODO
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyingPrice = buyingPrice;
+        this.currentPrice = buyingPrice;
+        this.highestPrice = buyingPrice;
     }
 
     // TODO: Update current market price
     public void updatePrice(double newPrice) {
-        // TODO
+        if(newPrice>0) {
+            currentPrice = newPrice;
+            if(newPrice>currentPrice){
+                highestPrice=newPrice;
+            }
+        }
     }
 
     // TODO: Calculate total profit or loss
     public double calculateProfit() {
         // TODO
-        return 0.0;
+        return (currentPrice-buyingPrice)*quantity;
     }
 
     // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
         // TODO
-        return 0.0;
+        return currentPrice*quantity;
     }
 }
